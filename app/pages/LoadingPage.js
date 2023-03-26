e5347783a22062240c88a56d1a3c5e7a6622ea15
carl ameline <solymnos@gmail.com>
2023-03-27 00:03:48 +0200

    add animation on loading page

--- app/pages/LoadingPage.js
+++ app/pages/LoadingPage.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import { Text, View, Image } from 'react-native';
+import React, { useRef , useEffect } from 'react';
+import { Text, View, Image, Animated } from 'react-native';
 
 export default function LoadingPage() {
+
+    const fallAnim = useRef(new Animated.Value(-180)).current;
+    const rotateAnim = useRef(new Animated.Value(0.5)).current;
+
+    useEffect(() => {
+        const fall = Animated.spring(fallAnim, {
+            toValue : 0,
+            duration: 1400,
+            useNativeDriver : true,
+            friction : 3,
+            tension : 0,
+        });
+
+        Animated.parallel([fall]).start();
+;
+    }, []);
+
+    const trans = {
+        transform : [
+            {
+                translateY : fallAnim
+            }
+        ]
+    }
 
     return (
         <View className="flex-1 items-center justify-center bg-black">
             <View className="flex h-2/3 w-full justify-center items-center">
-                <View className="flex h-1/2 w-full justify-center items-center">
+                <Animated.View style={[trans]} className="flex h-1/2 w-full justify-center items-center">
                     <Image style={{resizeMode: "contain"}} className='h-5/6 w-5/6 mt-10'   source={require('../assets/slylogo.png')}/>
-                </View>
+                </Animated.View>
             </View>
             <View className="flex h-1/3 w-full">
                 <Text className="text-black">Just test</Text>
---
+++ app/utils/ApiUtils.js
@@ -0,0 +1,5 @@
+export async function apiGetInfo()
+{
+    const delay = new Promise(res => setTimeout(res, 5000));
+    await delay;
+}
